e206919acc7894ac5f7d45680adc03fea94d9f54
Levi Munyon <lgmunyon@hotmail.com>
2022-11-09 13:35:15 -0500

    toLowerCase

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/', (request, response)=>{
 })
  
 app.get('/api/:bodyPart', (request, response)=>{
-    const bodyParts = request.params.bodyPart.toLowerCase()
+    const bodyParts = request.params.bodyPart
     if (stretches[bodyParts]){
         response.json(stretches[bodyParts])
     }
